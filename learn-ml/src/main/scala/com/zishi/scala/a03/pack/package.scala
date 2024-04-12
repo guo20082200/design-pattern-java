b3cd723fbd4ae44c0b7e4e1fb2dd803706420cda
guo20082200 <guo20082200@163.com>
2024-04-12 13:33:26 +0800

    111
    rtdrt

--- learn-ml/src/main/scala/com/zishi/scala/a03/pack/package.scala
+++ learn-ml/src/main/scala/com/zishi/scala/a03/pack/package.scala
@@ -12,3 +12,11 @@ package object MyPackageObject {
     println("hh")
   }
 }
+
+和 Java 一样，可以在顶部使用 import 导入，在这个文件中的所有类都可以使用。
+局部导入：什么时候使用，什么时候导入。在其作用范围内都可以使用。
+通配符导入：import java.util._
+给类起名：import java.util.{ArrayList=>JL}
+导入相同包的多个类：import java.util.{HashSet, ArrayList}
+屏蔽类：import java.util.{ArrayList =>_,_}
+导入包的绝对路径：new _root_.java.util.HashMap
