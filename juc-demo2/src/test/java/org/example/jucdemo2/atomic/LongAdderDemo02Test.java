92dfdd57dce42500f1f82b97bb6c8ecf6b544731
guo20082200 <guo20082200@163.com>
2024-11-26 16:43:37 +0800

    synchronized 和 AtomicInteger 和 LongAdder 和 LongAccumulator 对比

--- juc-demo2/src/test/java/org/example/jucdemo2/atomic/LongAdderDemo02Test.java
+++ juc-demo2/src/test/java/org/example/jucdemo2/atomic/LongAdderDemo02Test.java
@@ -5,10 +5,12 @@ import com.google.common.base.Stopwatch;
 import java.time.Duration;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.LongAccumulator;
 import java.util.concurrent.atomic.LongAdder;
 
 /**
- * LongAdder
+ * synchronized 和 AtomicInteger 和 LongAdder 和 LongAccumulator 对比
+ *
  */
 public class LongAdderDemo02Test {
 
@@ -23,8 +25,10 @@ public class LongAdderDemo02Test {
             new Thread(() -> {
                 try {
                     for (int i1 = 0; i1 < 1000000; i1++) {
-                        //b.addOne();
-                        b.addOne2();
+                        b.addOne0();
+                        //b.addOne1();
+                        //b.addOne2();
+                        //b.addOne3();
                     }
                 } finally {
                     countDownLatch.countDown();
@@ -34,25 +38,42 @@ public class LongAdderDemo02Test {
 
         countDownLatch.await();
         Duration elapsed = started.elapsed();
-        //System.out.println(b.atomicInteger.get()); // 100000000
-        //System.out.println(elapsed.getNano()); // 695510800
+        System.out.println(b.num); // 100000000
+        System.out.println("synchronized:" + elapsed.getNano()/1000000); // synchronized:697
 
-        System.out.println(b.adder.sum()); //100000000
-        System.out.println(elapsed.getNano()); // 255769600
+        //System.out.println(b.atomicInteger.get()); // 100000000
+        //System.out.println("atomicInteger: " + elapsed.getNano()/1000000); //atomicInteger:  926
+
+        //System.out.println(b.adder.sum()); //100000000
+        //System.out.println("adder:" + elapsed.getNano()/1000000); // adder:213
+
+
+        //System.out.println(b.accumulator.longValue()); //100000000
+        //System.out.println("accumulator: " + elapsed.getNano()/1000000); //accumulator:  174 ms
     }
 
 }
 
 class B {
-    public LongAdder adder = new LongAdder();
+    public int num = 0;
     public AtomicInteger atomicInteger = new AtomicInteger(0);
+    public LongAdder adder = new LongAdder();
+    public LongAccumulator accumulator = new LongAccumulator(Long::sum, 0);
 
-    public void addOne() {
+    public synchronized void addOne0() {
+        num++;
+    }
+
+    public void addOne1() {
         atomicInteger.incrementAndGet();
     }
 
     public void addOne2() {
         adder.increment();
+    }
+
+    public void addOne3() {
+        accumulator.accumulate(1);
     }
 
 }
