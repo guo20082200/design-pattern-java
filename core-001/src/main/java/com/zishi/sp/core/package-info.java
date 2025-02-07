c11de563ddfd9c3bc46bee0636993aa167bb4828
zishi <zishi@qq.com>
2025-02-07 16:53:32 +0800

    spark

---
+++ core-001/src/main/java/com/zishi/sp/core/package-info.java
@@ -0,0 +1,1 @@
+package com.zishi.sp.core;
---
+++ learn-ml/src/main/scala/com/zishi/scala/Test02.scala
@@ -0,0 +1,12 @@
+package com.zishi.scala
+
+object Test02 {
+  def main(args: Array[String]): Unit = {
+
+    println(23)
+
+    def test(): Int = {
+      3
+    }
+  }
+}
